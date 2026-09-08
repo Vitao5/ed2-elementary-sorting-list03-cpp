da4812dbae14c16ca29635303c1cc6eab15e407d
Victor Gabriel <69489641+Vitao5@users.noreply.github.com>
2026-09-07 22:40:07 -0300

    Refactor bubble sort to use user input
    Refactored bubble sort implementation to read input from user and output sorted array with swap count.

--- src/ex01-bubbleSorting.cpp
+++ src/ex01-bubbleSorting.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <utility>
 
 using namespace std;
-
-using ll = long long;
 
 #define IOFAST() ios_base::sync_with_stdio(0); cin.tie(0);
 
-using namespace std;
-class Solution {
-public:
-    pair<vector<int>, ll> bubbleSortSwapCount(vector<int> list) {
-        int countSwap = 0;
-        vector<int> vectorList = list;
-        int sizeVetor  = list.size();
-
-        for (int i = 0; i < sizeVetor -1; i++) {
-            for (int j = 0; j < sizeVetor -i -1; j++) {
-                if (vectorList[j] > vectorList[j + 1]) {
-                    int temp = vectorList[j];
-
-                    vectorList[j] = vectorList[j + 1];
-                    vectorList[j + 1] = temp;
-                    countSwap++;
-                }
-            }
-        }
-        cout << countSwap << endl;
-        for (int i = 0; i < vectorList.size(); i++) {
-            cout << vectorList[i] << " ";
-        }
-        return {list, 0};
-    }
-};
-
 int main() {
     IOFAST();
-    bubbleSortSwapCount({1,4,3,5});
+
+    int n;
+    cin >> n;
+
+    vector<int> vet(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vet[i];
+    }
+
+    long long trocas = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (vet[j] > vet[j + 1]) {
+                int temp = vet[j];
+                vet[j] = vet[j + 1];
+                vet[j + 1] = temp;
+                trocas++;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << vet[i] << (i == n - 1 ? "" : " ");
+    }
+    cout << "\n";
+    cout << trocas << "\n";
+
     return 0;
 }
