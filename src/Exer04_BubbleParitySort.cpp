d745e1928277501760284ed4eea09fb39d3d707d
Victor Gabriel <69489641+Vitao5@users.noreply.github.com>
2026-09-07 22:49:23 -0300

    Refactor bubble sort into Solution class method

--- src/Exer04_BubbleParitySort.cpp
+++ src/Exer04_BubbleParitySort.cpp
@@ -1,45 +1,35 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
-#define IOFAST() ios_base::sync_with_stdio(0); cin.tie(0);
-
-bool deveTrocar(int a, int b) {
-    bool aPar = a % 2 == 0;
-    bool bPar = b % 2 == 0;
-
-    if (aPar && !bPar) return false;
-    if (!aPar && bPar) return true;
-    if (aPar && bPar) return a > b;
-    return a < b;
-}
-
-int main() {
-    IOFAST();
-
-    int n;
-    cin >> n;
-
-    vector<int> vet(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vet[i];
+class Solution {
+public:
+    bool deveTrocar(int a, int b) {
+        bool aPar = a % 2 == 0;
+        bool bPar = b % 2 == 0;
+        if (aPar && !bPar) return false;
+        if (!aPar && bPar) return true;
+        if (aPar && bPar) return a > b;
+        return a < b;
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (deveTrocar(vet[j], vet[j + 1])) {
-                int temp = vet[j];
-                vet[j] = vet[j + 1];
-                vet[j + 1] = temp;
+    vector<int> bubbleParitySort(vector<int> nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (deveTrocar(nums[j], nums[j + 1])) {
+                    swap(nums[j], nums[j + 1]);
+                }
             }
         }
+        return nums;
     }
-
-    for (int i = 0; i < n; i++) {
-        cout << vet[i] << (i == n - 1 ? "" : " ");
+};
+int main() {
+    Solution sol;
+    vector<int> nums = {5, 2, 9, 8, 1, 6, 3, 4};
+    vector<int> resultado = sol.bubbleParitySort(nums);
+    for (int i = 0; i < resultado.size(); i++) {
+        cout << resultado[i] << (i == resultado.size() - 1 ? "" : " ");
     }
     cout << "\n";
-
     return 0;
 }
