dd31de1e81eeef1968b63021464d53990e9a72c7
Victor Gabriel <69489641+Vitao5@users.noreply.github.com>
2026-09-07 22:48:34 -0300

    Refactor bubble sort into Solution class
    Refactored bubble sort implementation into a class method and added swap count functionality.

--- src/ex01-bubbleSorting.cpp
+++ src/ex01-bubbleSorting.cpp
@@ -1,39 +1,29 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
-#define IOFAST() ios_base::sync_with_stdio(0); cin.tie(0);
-
-int main() {
-    IOFAST();
-
-    int n;
-    cin >> n;
-
-    vector<int> vet(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vet[i];
-    }
-
-    long long trocas = 0;
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (vet[j] > vet[j + 1]) {
-                int temp = vet[j];
-                vet[j] = vet[j + 1];
-                vet[j + 1] = temp;
-                trocas++;
+class Solution {
+public:
+    pair<vector<int>, long long> bubbleSortSwapCount(vector<int> nums) {
+        long long trocas = 0;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums[j], nums[j + 1]);
+                    trocas++;
+                }
             }
         }
+        return {nums, trocas};
     }
-
-    for (int i = 0; i < n; i++) {
-        cout << vet[i] << (i == n - 1 ? "" : " ");
+};
+int main() {
+    Solution sol;
+    vector<int> nums = {4, 3, 2, 1, 5};
+    pair<vector<int>, long long> resultado = sol.bubbleSortSwapCount(nums);
+    for (int i = 0; i < resultado.first.size(); i++) {
+        cout << resultado.first[i] << (i == resultado.first.size() - 1 ? "" : " ");
     }
-    cout << "\n";
-    cout << trocas << "\n";
-
+    cout << "\n" << resultado.second << "\n";
     return 0;
 }
